29ccfedd996635554298c8f3e3fb52f3801cb45f
po boyd <poboyduk@gmail.com>
2020-06-09 09:14:59 +0100

    added licence data

--- src/main/java/poshowtubelite/VideoExport.java
+++ src/main/java/poshowtubelite/VideoExport.java
@@ -1,5 +1,32 @@
 package main.java.poshowtubelite;
 
+/**
+ * Video Export
+ * Simple video file exporter.
+ * https://funprogramming.org/VideoExport-for-Processing
+ * <p>
+ * Copyright (c) 2017 Abe Pazos http://hamoid.com
+ * <p>
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307 USA
+ *
+ * @author Abe Pazos http://hamoid.com
+ * @modified 02/28/2018
+ * @version 0.2.3 (23)
+ */
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
